8e65b25bf87e8c47fdf5e76f371fb8560be4b4ca
Jonathan Franklin <jonathan.t.franklin@protonmail.com>
2018-11-13 13:43:52 -0700

    Added UI to BoundingBox.py

--- Scripts/BoundingBox.py
+++ Scripts/BoundingBox.py
@@ -1,5 +1,26 @@
 import maya.cmds as cmds
 
+def WindowMaker ():
+    mWin = "LocWindow"
+
+    if cmds.window(mWin, exists=True):
+        cmds.deleteUI(mWin)
+
+    mWin = cmds.window(mWin, title='Create Locator')
+    mCol = cmds.columnLayout(parent=mWin, adjustableColumn=True)
+    #dropCtrl = cmds.optionMenuGrp(parent=mCol, label='Type')
+    #cmds.menuItem(parent=dropCtrl, label='Bounding Box')
+    #cmds.menuItem(parent=dropCtrl, label='Pivot Point')
+    #cmds.button(parent=mCol, label='Create Locator')
+
+    cmds.text(parent=mCol, label='Locator Type:')
+    cmds.button(parent=mCol, label='Bounding Box', command=lambda *args: CreateLoc(1)) #Lambda function lets you put a full command with arguments in a button.
+    cmds.button(parent=mCol, label='Pivot Point', command=lambda *args: CreateLoc(2))
+
+    cmds.showWindow(mWin)
+
+WindowMaker ()
+
 def CreateLoc(option):
     sels = cmds.ls( sl = True )
 
@@ -19,4 +40,4 @@ def CreateLoc(option):
             loc = cmds.spaceLocator()
             cmds.xform(loc, translation = pivot, worldSpace = True)
 
-CreateLoc(1)
+#CreateLoc(1)
